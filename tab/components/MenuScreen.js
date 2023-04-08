9f4d0b31907ba7b21e257bd7e971ae22b060c14b
Rebecca Rozansky <32803510+rebeccarozansky@users.noreply.github.com>
2023-04-08 13:34:32 -0400

    filters out items

--- tab/components/MenuScreen.js
+++ tab/components/MenuScreen.js
@@ -3,25 +3,66 @@ import { View, Text, StyleSheet } from 'react-native';
 import menus from "./chmenus.json"
 import { ScrollView } from 'react-native';
 import MenuItem from './MenuItem';
+import foodres from "./foodres.json";
+
+function safefilter(item) {
+  meatlist = ["chicken","beef","pork","fish","tuna","yellowtail","salmon","pepperoni","bacon"]
+  user = "user"
+  restrictions = foodres[user]["food restriction"]
+  console.log(restrictions.includes("vegetarian"))
+  if(restrictions.includes("vegetarian")){
+    console.log("here!")
+    for(let i=0;i<meatlist.length;i++){
+      meat = meatlist[i];
+      console.log(item[0]["Description"])
+      console.log(meat)
+      if(item[0]["Description"].includes(meat)){
+        return false
+      }
+    }
+  }
+  for(let i=0;i<restrictions.length;i++){
+    res = restrictions[i];
+    if(res == "vegetarian" && (item[0]["Description"].includes(res))){
+      //console.log(item)
+
+      return true;
+    }
+    else if(item[0]["Description"].includes(res)){
+      return false;
+    }
+  }
+  return true;
+  // your implementation of safefilter function
+}
 
 export default function MenuScreen({route}) {
   var parn = ""
   var c = 0
   menu = []
+  const safefoods = {};
+
   if(typeof route.params !== 'undefined'){
-    //console.log(Object.keys(route.params))
     par = route.params
     parn = par.name
     oof = "Top Of The Hill Restaurant & Brewery"
-    //console.log(Object.keys(menus))
-    //console.log(menus[parn])
+
     if(menus.hasOwnProperty(parn)){
       menu = menus[parn]
-      console.log("here!")
-      console.log(menu)
+      //console.log("here!")
+      //console.log(menu)
       menukeys = menu.keys
       
     }
+
+
+      for (let item in menu) {
+        if (safefilter(menu[item])) {
+          safefoods[item] = menu[item];
+          
+        }
+      }
+    
   }
 
 
@@ -54,6 +95,16 @@ Object.keys(menu).map(item => {
 })
 
 */}
+  <ScrollView>
+
+{Object.keys(safefoods).map(item => {
+  return safefoods[item].map(({ Description, Price }) => {
+    return <MenuItem name={item} description={Description} price = {Price}/>
+  });
+})
+}
+</ScrollView>
+
   <ScrollView>
 
 {Object.keys(menu).map(item => {
