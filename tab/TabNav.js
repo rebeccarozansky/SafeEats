3e4255ad8c8683e6381e6859ff2bc07493e22175
mmnasser2000 <mmnasser2000@gmail.com>
2023-04-22 23:13:38 -0400

    Color choices for nav bar

--- tab/TabNav.js
+++ tab/TabNav.js
@@ -1,96 +1,102 @@
-import { TextInput, Text, View, StyleSheet, Button, TouchableOpacity, Image } from 'react-native';
-import React, {Component, useState} from "react";
-import { color } from '@rneui/themed/dist/config';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import UserProfile from './user/UserProfile';
-import HomeScreen from './home/HomeScreen';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
-import { AntDesign } from '@expo/vector-icons';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import MenuScreen from './components/MenuScreen';
-import StackNav from './home/StackNav';
+import {
+  TextInput,
+  Text,
+  View,
+  StyleSheet,
+  Button,
+  TouchableOpacity,
+  Image,
+} from "react-native";
+import React, { Component, useState } from "react";
+import { color } from "@rneui/themed/dist/config";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import UserProfile from "./user/UserProfile";
+import HomeScreen from "./home/HomeScreen";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { AntDesign } from "@expo/vector-icons";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import MenuScreen from "./components/MenuScreen";
+import StackNav from "./home/StackNav";
 
 const Tab = createBottomTabNavigator();
 
 const Stack = createNativeStackNavigator();
 
-
-
-
 export default function TabNav() {
-  
-    return (
-      <Tab.Navigator
+  return (
+    <Tab.Navigator
       screenOptions={{
         headerShown: false,
-        tabBarActiveTintColor: "#F00"
-      }}>
-          <Tab.Screen 
-            name="Home" 
-            component={StackNav}
-            options={{
-              tabBarLabel: 'Home',
-              tabBarIcon: ({ color, size }) => (
-              <MaterialCommunityIcons name="silverware-fork-knife" size={24} color="3B3B3B33" />
-              ),
-            }} 
-          />
-          <Tab.Screen 
-            name="UserProfile" 
-            component={UserProfile} 
-            options={{
-              tabBarLabel: 'User Profile',
-              tabBarIcon: ({ color, size }) => (
-                <AntDesign name="profile" size={24} color="black" />              ),
-            }} 
-          />
-        </Tab.Navigator>
-      );
+        tabBarActiveTintColor: "#F00",
+      }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={StackNav}
+        options={{
+          tabBarLabel: "Home",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons
+              name="silverware-fork-knife"
+              size={24}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="UserProfile"
+        component={UserProfile}
+        options={{
+          tabBarLabel: "User Profile",
+          tabBarIcon: ({ color, size }) => (
+            <AntDesign name="profile" size={24} color={color} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
 }
 
-
-
 const styles = StyleSheet.create({
-    allergiesView: {
-        width: "100%",
-    }, 
-    title: {
-        fontSize: 16,
-        marginLeft: "7%",
-        fontWeight: "bold"
-    },
-    checkboxContainer: {
-      marginBottom: 20,
-      flexDirection: 'row',
-  
-    },
-    checkboxRowContainer: {
-        flexDirection: "row",
-        alignItems: "center"
-    },
-    allergiesData: {
-        marginTop: 5,
-        marginLeft: "5%",
-        width: "90%",
-        flexDirection: "row",
+  allergiesView: {
+    width: "100%",
+  },
+  title: {
+    fontSize: 16,
+    marginLeft: "7%",
+    fontWeight: "bold",
+  },
+  checkboxContainer: {
+    marginBottom: 20,
+    flexDirection: "row",
+  },
+  checkboxRowContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  allergiesData: {
+    marginTop: 5,
+    marginLeft: "5%",
+    width: "90%",
+    flexDirection: "row",
 
-        borderColor: "#3B3B3B33",
-        borderWidth: 2,
-        borderRadius: 10,
-    },
-    checkboxColumn: {
-      flexDirection: 'column',
-      margin: 2,
-    },
-    checkboxRow: {
-      margin: 5,
-      fillColor: "red",
-      unfillColor: "#FFFFFF",
-    },
-    icon: {
-        height: 20,
-        width: 20,
-        resizeMode:'contain'
-    }
-  });
-  
+    borderColor: "#3B3B3B33",
+    borderWidth: 2,
+    borderRadius: 10,
+  },
+  checkboxColumn: {
+    flexDirection: "column",
+    margin: 2,
+  },
+  checkboxRow: {
+    margin: 5,
+    fillColor: "red",
+    unfillColor: "#FFFFFF",
+  },
+  icon: {
+    height: 20,
+    width: 20,
+    resizeMode: "contain",
+  },
+});
