9c14b8b78690471fda0c08b5508d46e1fd7675cc
mmnasser2000 <mmnasser2000@gmail.com>
2023-04-22 23:13:22 -0400

    Fixed navigating to a restaurant menu

--- tab/home/Partner.js
+++ tab/home/Partner.js
@@ -1,52 +1,56 @@
-import { TextInput, Text, View, StyleSheet, Image, TouchableOpacity } from 'react-native';
-import React, {Component, useState} from "react";
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import RestaurantScreen from '../restaurant/RestaurantScreen';
-
-const Stack = createNativeStackNavigator();
+import {
+  TextInput,
+  Text,
+  View,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+import React, { Component, useState } from "react";
+import { useNavigation } from '@react-navigation/native';
+import RestaurantScreen from "../restaurant/RestaurantScreen";
 
 
 export default function Partner(props) {
-  
-    return (
-      <View style={styles.container}>
-        <TouchableOpacity onPress={()=>{props.navigation.navigate("Restaurant", {url: props.url,
-                                                                                 title: props.title,
-                                                                                 address: props.address,
-                                                                                 price:"$",
-                                                                                 cuisine:"American" })}}>
-          <Image style={styles.img} source={props.url} />
-          <Text style={styles.title}>{props.title}</Text>
-          <Text style={styles.address}>{props.address}</Text>
-        </TouchableOpacity>
-      </View>
-    );
+    const navigation = useNavigation(); 
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity
+        onPress={() => {
+          navigation.navigate("MenuScreen", {
+            name: props.title,
+          });
+        }}
+      >
+        <Image style={styles.img} source={props.url} />
+        <Text style={styles.title}>{props.title}</Text>
+        <Text style={styles.address}>{props.address}</Text>
+      </TouchableOpacity>
+    </View>
+  );
 }
-
-
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#F5F5F5',
+    backgroundColor: "#F5F5F5",
     height: 250,
     paddingLeft: 10,
-    paddingRight: 10
+    paddingRight: 10,
   },
   img: {
     height: 200,
     maxWidth: 400,
     borderRadius: 15,
-    resizeMethod: "scale"
+    resizeMethod: "scale",
   },
   title: {
     fontSize: 22,
-    marginLeft: 10
+    marginLeft: 10,
   },
   address: {
     fontSize: 16,
     marginLeft: 10,
-    color: "gray"
-  }
-  });
-  
+    color: "gray",
+  },
+});
